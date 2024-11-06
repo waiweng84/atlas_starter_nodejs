cd1f33b8aac0a9f440549f4370cbaaa4e22900d9
waiweng84 <65702453+waiweng84@users.noreply.github.com>
2024-11-06 23:00:16 +1100

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ async function run() {
   // Altas cluster specifics. Be sure it includes
   // a valid username and password! Note that in a production environment,
   // you do not want to store your password in plain-text here.
-  const uri =
-    "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
+  const uri = process.env.mongodb_uri
+  //"mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
 
   // The MongoClient is the object that references the connection to our
   // datastore (Atlas, for example)
